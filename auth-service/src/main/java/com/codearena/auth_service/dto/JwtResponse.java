ff26e3f3e0eb52a8d705a525534dbe2d6e6eae5f
Ambar Mishra <mishraambar19@gmail.com>
2025-09-05 23:49:50 +0530

    feat(auth): add JwtResponse DTO to send JWT token and user details

---
+++ auth-service/src/main/java/com/codearena/auth_service/dto/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.codearena.auth_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private String username;
+    private String role;
+}
