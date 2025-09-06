2e8c47b6ae88242bb3b18be497204f0f82415b98
Ambar Mishra <mishraambar19@gmail.com>
2025-09-06 08:00:53 +0530

    feat(mapper): implement ProblemMapper to convert between entity and DTOs

---
+++ problem-service/src/main/java/com/codearena/problem_service/mapper/ProblemMapper.java
@@ -0,0 +1,34 @@
+package com.codearena.problem_service.mapper;
+
+import com.codearena.problem_service.dto.*;
+import com.codearena.problem_service.entity.Problem;
+
+public class ProblemMapper {
+
+    public static Problem toEntity(ProblemRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return Problem.builder()
+                .title(request.getTitle())
+                .description(request.getDescription())
+                .inputFormat(request.getInputFormat())
+                .outputFormat(request.getOutputFormat())
+                .difficulty(request.getDifficulty())
+                .build();
+    }
+
+    public static ProblemResponse toResponse(Problem problem) {
+        if (problem == null) {
+            return null;
+        }
+        return ProblemResponse.builder()
+                .id(problem.getId())
+                .title(problem.getTitle())
+                .description(problem.getDescription())
+                .inputFormat(problem.getInputFormat())
+                .outputFormat(problem.getOutputFormat())
+                .difficulty(problem.getDifficulty())
+                .build();
+    }
+}
