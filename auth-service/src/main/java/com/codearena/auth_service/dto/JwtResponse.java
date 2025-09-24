f6f0b2ca22198aaf75c7b6e001e7a97834b9c3d3
Ambar Mishra <mishraambar19@gmail.com>
2025-09-24 23:50:36 +0530

    debugging JwtResponse

--- auth-service/src/main/java/com/codearena/auth_service/dto/JwtResponse.java
+++ auth-service/src/main/java/com/codearena/auth_service/dto/JwtResponse.java
@@ -2,12 +2,16 @@ package com.codearena.auth_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 public class JwtResponse {
     private String token;
-    private String type = "Bearer";
+    private Long id;
     private String username;
-    private String role;
+    private String email;
+    private Collection<? extends GrantedAuthority> roles;
 }
