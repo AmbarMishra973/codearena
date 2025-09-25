7965ff85f9e33e6217726b1d425c6c4893081b07
Ambar Mishra <mishraambar19@gmail.com>
2025-09-25 23:48:21 +0530

    debugging SubmissionController

--- submission-service/src/main/java/com/codearena/submission_service/controller/SubmissionController.java
+++ submission-service/src/main/java/com/codearena/submission_service/controller/SubmissionController.java
@@ -1,26 +1,31 @@
 package com.codearena.submission_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.codearena.submission_service.service.SubmissionService;
 import com.codearena.submission_service.dto.SubmissionRequest;
 import com.codearena.submission_service.dto.SubmissionResponse;
+import com.codearena.submission_service.service.SubmissionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/submissions")
 public class SubmissionController {
 
-    @Autowired
-    private SubmissionService service;
+    private final SubmissionService submissionService;
 
-    @PostMapping
-    public ResponseEntity<SubmissionResponse> submit(@RequestBody SubmissionRequest request) {
-        SubmissionResponse response = service.submitCode(request);
+    public SubmissionController(SubmissionService submissionService) {
+        this.submissionService = submissionService;
+    }
+
+    @PostMapping("/api/submissions/{contestId}")
+    public ResponseEntity<SubmissionResponse> submitCode(
+            @PathVariable Long contestId,
+            @RequestHeader("userId") String userId,
+            @RequestBody SubmissionRequest submissionRequest) {
+
+        // Set contestId and userId in the request object
+        submissionRequest.setContestId(contestId);
+        submissionRequest.setUserId(Long.parseLong(userId));  // Convert userId to Long, if necessary
+
+        SubmissionResponse response = submissionService.submitCode(submissionRequest);
         return ResponseEntity.ok(response);
     }
 }
