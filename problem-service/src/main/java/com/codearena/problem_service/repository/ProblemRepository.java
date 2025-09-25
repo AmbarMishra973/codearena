1be2a6beba8894da0acde930303e59b7db484fde
Ambar Mishra <mishraambar19@gmail.com>
2025-09-25 23:47:51 +0530

    debugging ProblemRepository

--- problem-service/src/main/java/com/codearena/problem_service/repository/ProblemRepository.java
+++ problem-service/src/main/java/com/codearena/problem_service/repository/ProblemRepository.java
@@ -2,8 +2,15 @@ package com.codearena.problem_service.repository;
 
 import com.codearena.problem_service.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+
+    // Custom query to fetch all problem IDs
+    @Query("SELECT p.id FROM Problem p")
+    List<Long> findAllProblemIds();
 }
