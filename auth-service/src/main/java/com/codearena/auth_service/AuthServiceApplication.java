2eb55671575637563d6ad375bdf0dd7df79b3a43
Ambar Mishra <mishraambar19@gmail.com>
2025-09-27 23:33:34 +0530

    adding  annotations in main

--- auth-service/src/main/java/com/codearena/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/codearena/auth_service/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.codearena.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "com.codearena.auth_service")
+@EnableEurekaClient
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
